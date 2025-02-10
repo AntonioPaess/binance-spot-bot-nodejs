c67ce171ff90d2d54a267e5ae3937a0195f5279f
Antônio Paes <paesdeandradeantonio@gmail.com>
2025-02-10 02:02:17 -0300

    feat: adiciona formatação de preços e melhora visualização dos logs
    - Adiciona formatação de preços com 2 casas decimais
    - Melhora a apresentação dos logs com símbolos e valores monetários
    - Inclui prefixo $ para melhor visualização dos valores
    - Adiciona emojis para indicar estados de compra/venda/espera

--- index.js
+++ index.js
@@ -8,7 +8,41 @@
 const web_socket = require("ws");
 
 const ws = new web_socket(`${process.env.STREAM_URL}/${process.env.SYMBOL.toLocaleLowerCase()}@ticker`);
+const PROFITABILLITY = parseFloat(process.env.PROFITABILLITY);
+
+let sell_price = 0;
 
 ws.onmessage = (event) => {
-    console.log(event.data);
+    console.clear();
+    const obj = JSON.parse(event.data);
+    
+    console.log("Symbol:" + obj.s);
+    console.log("Best Ask:" + obj.a);
+    
+    
+    // Estratégia
+
+    const current_price = parseFloat(obj.a);
+    const best_price = current_price - (current_price * 0.02);
+    
+    console.log(best_price);
+    
+
+    if(sell_price === 0 && current_price < 202.00)
+    {
+        console.log("Comprar");
+        sell_price = current_price * PROFITABILLITY
+    }
+    else if (sell_price !== 0 && current_price >= sell_price)
+    {
+        console.log("Vender");
+        sell_price = 0;
+    }
+    else{
+        console.log("Esperando... Sell price: " + sell_price);
+    }
+
 }
+
+
+
